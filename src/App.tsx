5d9b9880e18cf171e1e42e5a7cf60087570a586b
jeromeoe <jeromepooi@gmail.com>
2026-01-19 00:18:26 +0800

    Resolved merge conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-function App() {
-  return (
-    <div className="min-h-screen p-8">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import VolunteerHome from "./pages/VolunteerHome";
+import VolunteerCommitments from "./pages/VolunteerCommitments";
+import Login from "./pages/Login";
 
-export default App
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected area */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Navigate to="/volunteer" replace />} />
+        <Route path="/volunteer" element={<VolunteerHome />} />
+        <Route path="/volunteer/commitments" element={<VolunteerCommitments />} />
+      </Route>
+
+      {/* fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
